e35100445fd3e458cc301c1117fe85c1abdac224
j84077200345 <j84077200345@gmail.com>
2020-07-08 20:09:26 +0800

    Updates

---
+++ precache-manifest.815f2c081534ea978fa348c8942d6226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94541641cf22fea2d4f391e6e9d5126",
+    "url": "/love-story/index.html"
+  },
+  {
+    "revision": "73818614f3e908039356",
+    "url": "/love-story/static/css/main.6d138a17.chunk.css"
+  },
+  {
+    "revision": "cdc419a811622ecb3bde",
+    "url": "/love-story/static/js/2.72f99cbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/love-story/static/js/2.72f99cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73818614f3e908039356",
+    "url": "/love-story/static/js/main.6070ade7.chunk.js"
+  },
+  {
+    "revision": "b363eed0b18bd09ddfd6",
+    "url": "/love-story/static/js/runtime-main.583c57d6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/love-story/precache-manifest.815f2c081534ea978fa348c8942d6226.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/love-story/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
